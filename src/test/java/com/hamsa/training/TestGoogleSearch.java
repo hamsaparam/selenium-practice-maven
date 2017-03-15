4225167bc5778a33f5202476661fcd97fcf67b42
hamsaparam <catchme.hamsa@gmail.com>
2017-03-14 22:36:22 -0500

    Initial Commit

---
+++ src/test/java/com/hamsa/training/TestGoogleSearch.java
@@ -0,0 +1,81 @@
+package com.hamsa.training;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.*;
+
+
+    public class TestGoogleSearch  extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestGoogleSearch( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TestGoogleSearch.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+
+
+		System.setProperty("webdriver.chrome.driver", "C:/Users/hamsa/Downloads/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+    	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    	        driver.get("http://www.google.com");
+    	        //Maximize the Browser window
+    	        driver.manage().window().maximize();
+
+    	        //Get the current page URL and store the value in variable 'str'
+    	        final String str = driver.getCurrentUrl();
+
+    	        //Print the value of variable in the console
+    	        System.out.println("The current URL is " + str);
+                driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+    	        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("coelacanths");
+    	        driver.findElement(By.xpath("//button[@name='btnG']")).click();
+    	        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+    	        driver.findElement(By.partialLinkText("Wikipedia")).click();
+    	        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
+    	        
+    	        String currentURL = null;
+                WebDriverWait wait = new WebDriverWait(driver, 10);
+    	        
+    	        ExpectedCondition e = new ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver d) {
+                    return (d.getCurrentUrl() != str);
+                    }
+                };
+
+                wait.until(e);
+                currentURL = driver.getCurrentUrl();
+                System.out.println("The current URL has changed to: " + currentURL);
+                
+    	        String str2 = driver.getCurrentUrl();
+    	        System.out.println("The current URL has changed to: " + str2);
+    
+            assertTrue( true );
+    }
+}
+
+
